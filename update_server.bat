a7fec581854cca1bdd5eb26d46863f7865e08752
Jesus <jesusdiaz200118@gmail.com>
2025-10-01 16:36:29 -0300

    Agregar scripts de Git para flujo de trabajo diario

---
+++ update_server.bat
@@ -0,0 +1,14 @@
+@echo off
+echo ========================================
+echo    ACTUALIZANDO PROYECTO POSTVENTA
+echo ========================================
+echo.
+
+cd C:\Users\Jesus Diaz\postventa-system
+echo Descargando cambios de GitHub...
+git pull
+
+echo.
+echo Proyecto actualizado exitosamente!
+echo.
+pause
